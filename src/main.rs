f6c956a202d612004acd72f557c1b283f478eb05
Jean <jean.vanwittenberge@gmail.com>
2024-10-01 16:46:51 +0000

    Modification du projet pour qu'il n'utilise pas la bibliothèque standard.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// Build le programme avec: RUSTFLAGS="-C link-arg=-nostartfiles" cargo build
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    loop {}
 }
