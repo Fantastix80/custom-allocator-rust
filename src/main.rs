66e621ea27f99c4b6bbe1e4a1bdab8f11b0ca474
Jean <jean.vanwittenberge@gmail.com>
2024-10-12 16:40:55 +0200

    Correction des bugs, le programme compile

--- src/main.rs
+++ src/main.rs
@@ -2,79 +2,83 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::ptr::null_mut;
 use core::alloc::{GlobalAlloc, Layout};
-use core::ptr::{null_mut, NonNull};
-
-//interrupt::free(|_| { /* code qui va pas s'interrompre */})
+use core::ptr::NonNull;
+use core::mem::size_of;
+use core::cell::UnsafeCell;
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> ! {
     loop {}
 }
 
-// Création de la structure de mon allocateur (modèle: free list allocator)
-struct MyAlloc {
-    free_list: Option<NonNull<FreeBlock>>,
-    pool_start: *mut u8, 
-    pool_size: usize,
-}
-
+// Structure pour représenter un bloc libre dans la free list
 struct FreeBlock {
-    size: usize,
-    next: Option<NonNull<FreeBlock>>, // Contient le prochain block libre
+    size: usize,                       // Taille du bloc libre
+    next: Option<NonNull<FreeBlock>>,   // Pointeur vers le bloc libre suivant
 }
 
-impl MyAlloc {
-    // Crée un nouvel allocateur avec un pool de mémoire donné
-    fn new(pool_start: *mut u8, pool_size: usize) -> Self {
-        MyAlloc {
-            free_list: None,
+// L'allocateur en lui-même, basé sur une free list
+pub struct FreeListAllocator {
+    free_list: UnsafeCell<Option<NonNull<FreeBlock>>>, // Liste chaînée des blocs libres
+    pool_start: *mut u8,                               // Début du pool de mémoire
+    pool_size: usize,                                  // Taille du pool de mémoire
+}
+
+impl FreeListAllocator {
+    /// Crée un nouvel allocateur avec un pool de mémoire donné
+    pub const fn new(pool_start: *mut u8, pool_size: usize) -> Self {
+        FreeListAllocator {
+            free_list: UnsafeCell::new(None),
             pool_start,
             pool_size,
         }
     }
 
-    // initialise l'allocateur avec un bloc unique couvrant toute la mémoire
-    unsafe fn init(&mut self) { // On le marque comme unsafe car la fonction write nécessite que ce soit spécifié
-        let first_block: FreeBlock = FreeBlock {
+    /// Initialise l'allocateur avec un bloc unique couvrant toute la mémoire
+    pub unsafe fn init(&self) {
+        let first_block = FreeBlock {
             size: self.pool_size,
             next: None,
         };
 
-        let first_block_ptr: *mut FreeBlock = self.pool_start as *mut FreeBlock;
+        let first_block_ptr = self.pool_start as *mut FreeBlock;
         first_block_ptr.write(first_block);
 
-        self.free_list = Some(NonNull::new(first_block_ptr).unwrap());
+        *self.free_list.get() = Some(NonNull::new(first_block_ptr).unwrap());
     }
 
-    fn find_fit(&mut self, layout: Layout)/* -> *mut u8 */ {
-        let mut prev = None;
-        let mut current = self.free_list;
+    /// Fonction pour trouver un bloc libre et l'allouer
+    unsafe fn find_fit(&self, layout: Layout) -> *mut u8 {
+        let mut prev: Option<NonNull<FreeBlock>> = None;
+        let mut current = *self.free_list.get();
 
         while let Some(mut current_block) = current {
             let block = current_block.as_mut();
-
-            if block.size > layout.size() + size_of::<FreeBlock>() {
-                // On retire ce bloc de la free list
+            
+            // Vérifier la taille et l'alignement
+            if block.size >= layout.size() + size_of::<FreeBlock>() && (current_block.as_ptr() as usize) % layout.align() == 0 {
+                // Retirer ce bloc de la free list
                 if let Some(mut prev_block) = prev {
                     prev_block.as_mut().next = block.next;
                 } else {
-                    self.free_list = block.next;
+                    *self.free_list.get() = block.next;
                 }
 
-                // On sépare la mémoire si elle est plus grande que l'élément qu'on stocke
+                // Séparer la mémoire si nécessaire
                 let remaining_size = block.size - layout.size() - size_of::<FreeBlock>();
 
                 if remaining_size >= size_of::<FreeBlock>() {
-                    // On crée un nouveau bloc libre avec le reste de la mémoire
-                    let new_free_block_ptr = (self.pool_start as usize + layout.size()) as *mut FreeBlock;
+                    // Créer un nouveau bloc libre avec le reste
+                    let new_free_block_ptr = (current_block.as_ptr() as usize + layout.size()) as *mut FreeBlock;
                     let new_free_block = FreeBlock {
                         size: remaining_size,
                         next: block.next,
-                    }
+                    };
 
                     new_free_block_ptr.write(new_free_block);
-                    self.free_list = Some(NonNull::new(new_free_block_ptr).unwrap());
+                    *self.free_list.get() = Some(NonNull::new(new_free_block_ptr).unwrap());
                 }
 
                 return current_block.as_ptr() as *mut u8;
@@ -88,25 +92,25 @@ impl MyAlloc {
         null_mut()
     }
 
-    // Fonction pour libérer un bloc de mémoire
-    fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+    /// Fonction pour libérer un bloc de mémoire
+    pub unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         let block_ptr = ptr as *mut FreeBlock;
 
-        // Réinsérer un bloc dans la free_list
+        // Réinsérer le bloc dans la free list
         let new_free_block = FreeBlock {
             size: layout.size(),
-            next: self.free_list,
+            next: *self.free_list.get(),
         };
 
         block_ptr.write(new_free_block);
-        self.free_list = Some(NonNull::new(block_ptr).unwrap());
+        *self.free_list.get() = Some(NonNull::new(block_ptr).unwrap());
 
-        // Fusionner les blocs adjaçants (pas implémenté encore)
+        // Fusionner avec les blocs adjacents si possible (pas implémenté ici)
     }
 }
 
-// Implémentation de GlobalAlloc dans notre allocateur custom
-unsafe impl GlobalAlloc for MyAlloc {
+// Implémentation de GlobalAlloc pour le free list allocator
+unsafe impl GlobalAlloc for FreeListAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         self.find_fit(layout)
     }
@@ -116,13 +120,7 @@ unsafe impl GlobalAlloc for MyAlloc {
     }
 }
 
-const POOL_SIZE: usize = 1024;
-static mut POOL_START: [u8; POOL_SIZE] = [0, POOL_SIZE];
-
-#[global_allocator]
-static A: MyAlloc = MyAlloc::new(POOL_START, POOL_SIZE);
-
 #[no_mangle]
-pub extern "C" fn _start() {
+pub extern "C" fn _start() -> ! {
     loop {}
 }
---
+++ target/debug/deps/custom_alloc_rust-0cafbf7acb3d3dd7.d
@@ -0,0 +1,5 @@
+/home/user/rust/custom_alloc/custom-allocator-rust/target/debug/deps/custom_alloc_rust-0cafbf7acb3d3dd7: src/main.rs
+
+/home/user/rust/custom_alloc/custom-allocator-rust/target/debug/deps/custom_alloc_rust-0cafbf7acb3d3dd7.d: src/main.rs
+
+src/main.rs:
